9dcde0d24aa63e8e3d00bb286de434e1a54136f7
Dave Abrahams <dave@boostpro.com>
2012-11-06 13:18:09 -0500

    Trivial bugfix

--- exercises/eh_test/sparse_vec_solution.cpp
+++ exercises/eh_test/sparse_vec_solution.cpp
@@ -1,8 +1,8 @@
 // Copyright David Abrahams 2008. Distributed under the Boost
 // Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
-#ifndef _GLIBCXX_DEBUG 1
+https://github.com/boostpro/bbn-2012-11/pulls
+#ifndef _GLIBCXX_DEBUG 
 // If this test runs too slowly for you, comment out the next line
 # define _GLIBCXX_DEBUG 1
 #endif
